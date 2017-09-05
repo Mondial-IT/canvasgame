e84ca670e88911b25c9417923b64714c22407fdf
rbx <robur@mondial-it.nl>
2017-09-05 16:49:03 +0200

    Html client server buttons added. Html moved to index.js

---
+++ index.js
@@ -0,0 +1,126 @@
+
+function htmlClient(){
+    document.getElementById("content").innerHTML = `
+        <table style="width: 100%">
+            <tr>
+                <td style="width: 30%">
+                    <h2>A fun canvas game</h2>
+                    <p>Controls</p>
+                    <table>
+                        <tr>
+                            <td style="font-weight: bold">Mouse</td>
+                            <td style="width: 20px"></td>
+                            <td>Accelerate in a direction</td>
+                        </tr>
+                        <tr>
+                            <td style="font-weight: bold">Left click</td>
+                            <td style="width: 20px"></td>
+                            <td>Teleport</td>
+                        </tr>
+                        <tr>
+                            <td style="font-weight: bold">Q</td>
+                            <td style="width: 20px"></td>
+                            <td>Shrink</td>
+                        </tr>
+                        <tr>
+                            <td style="font-weight: bold">W</td>
+                            <td style="width: 20px"></td>
+                            <td>Display hitbox mesh</td>
+                        </tr>
+                    </table>
+                </td>
+                <td style="width: 40%">
+                </td>
+                <td style="width: 40%">
+                    <table>
+                        <tr>
+                            <td>
+                                <table>
+                                    <tr>
+                                        <td>Render frame:</td>
+                                        <td>
+                                            <div id="renderframe">0</div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Physics frame:</td>
+                                        <td>
+                                            <div id="physicsframe">0</div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="vertical-align: top">
+                                            Frame time (ms):
+                                        </td>
+                                        <td>
+                                            <table style="border: 1px solid black">
+                                                <tr>
+                                                    <td>Physics</td>
+                                                    <td>
+                                                        <div id="frametimephysics">0</div>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Rendering</td>
+                                                    <td>
+                                                        <div id="frametimerendering">0</div>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Waiting</td>
+                                                    <td>
+                                                        <div id="frametimewaited">0</div>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Total</td>
+                                                    <td style="border-top: 1px solid black">
+                                                        <div id="frametime">0</div>
+                                                    </td>
+                                                </tr>
+                                                <tr style="color: gray">
+                                                    <td>Target</td>
+                                                    <td>
+                                                        <div id="targetframetime">0</div>
+                                                    </td>
+                                                </tr>
+                                                <tr style="color: gray">
+                                                    <td title="Total ms that was needed but not available for physics and rendering.">Sum time shortage</td>
+                                                    <td>
+                                                        <div id="timeshortage">0</div>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Player coordinates:</td>
+                                        <td><div id="player"></div></td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>`;
+}
+
+function htmlServer(){
+    document.getElementById("content").innerHTML = `
+        <div>Server running!</div>
+        <button onclick="resetGame()">Reset game</button>
+    `;
+}
+
+function initClient(){
+    document.title = "Client";
+    htmlClient();
+    startClient();
+}
+
+function initServer(){
+    document.title = "Server";
+    htmlServer();
+    startServer();
+}
--- server/server.js
+++ server/server.js
@@ -10,19 +10,6 @@ function startServer() {
     // Global so button events can get in
     simulator = new Simulator(level.player, level.scenery, level.hitboxes);
 
-    /**
-     * Puts server specific html on screen.
-     */
-    function editHtml(){
-        let htmlstr = "";
-        htmlstr += '<div>';
-        htmlstr += '<button onclick="resetGame()">Reset game</button>';
-        htmlstr += '</div>';
-
-        document.getElementById("tabtypespace").innerHTML = htmlstr;
-    }
-    editHtml();
-
     // Endless loop
     simulator.simulationLoop();
 }
